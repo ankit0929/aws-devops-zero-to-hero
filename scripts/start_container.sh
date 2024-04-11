23c542eaab6a171f2649f51c910704eb66bfcd84
ankit0929 <86465506+ankit0929@users.noreply.github.com>
2024-04-11 17:22:08 +0530

    added new code for code deploy again

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull ankit0929/second-ci-repo-for-testing:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 docker pull ankit0929/second-ci-repo-for-testing
+docker run -d -p 5000:5000 docker pull ankit0929/second-ci-repo-for-testing:latest
